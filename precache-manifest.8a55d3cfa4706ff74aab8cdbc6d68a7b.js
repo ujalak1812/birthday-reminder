a94ac1d87116b98b0994d14b3156b1ba9d2e65c3
ujalakhurram <ujala.khurram@venturedive.com>
2021-06-29 12:52:56 +0500

    Updates

---
+++ precache-manifest.8a55d3cfa4706ff74aab8cdbc6d68a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2929c9d35eeb55f2565f1769214cc2",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "a5675f2a059094b15d1b",
+    "url": "/birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5675f2a059094b15d1b",
+    "url": "/birthday-reminder/static/js/main.54028a4b.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
